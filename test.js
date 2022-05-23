d865ed77b5958348819dce750b9e223c3b0efded
Brandon Ong <brandonojy@outlook.sg>
2022-05-23 22:02:03 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 document.getElementById('cars').addEventListener('change', function() {
   console.log('You selected: ', this.value);
-  console.log(document.getElementById("cars").value);
+  console.log(document.querySelector("div.sgds-card:not(" + this.value + "])"));
 });
