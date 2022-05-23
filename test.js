698279db0133d0ba2359cd3321a44ed1a9a77f38
Brandon Ong <brandonojy@outlook.sg>
2022-05-23 22:15:15 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 document.getElementById('cars').addEventListener('change', function() {
   console.log('You selected: ', this.value);
-  var allcoysnotshown = document.querySelectorAll("div.sgds-card:not(" + this.value + ")")
-  for (x in allcoysnotshown) {
-    allcoysnotshown[x].style.display = "none"
-  }
+  document.querySelectorAll("div.sgds-card:not(" + this.value + ")").forEach(function(el) {
+    el.style.display = "none";
+  });
 });
