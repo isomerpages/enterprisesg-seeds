fb58ea88fb912c8b530d8ff12aabfdc9bf7afb32
Brandon Ong <brandonojy@outlook.sg>
2022-05-23 20:50:01 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-function onCoyChange() {
+document.getElementById('cars').addEventListener('change', function() {
+  console.log('You selected: ', this.value);
   console.log(document.getElementById("cars").value);
-}
+});
