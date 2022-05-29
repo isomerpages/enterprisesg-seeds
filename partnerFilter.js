7b953680c3cbcf186032a53784a0f34f0a65dde6
Brandon Ong <brandonojy@outlook.sg>
2022-05-29 17:16:13 +0800

    Create partnerFilter.js

---
+++ partnerFilter.js
@@ -0,0 +1,12 @@
+document.getElementById('partner-choice').addEventListener('change', function() {
+  console.log('You selected: ', this.value);
+  document.querySelectorAll("div.sgds-card").forEach(function(el) {
+    el.style.display = "block";
+  });
+  if (this.value != "all") {
+    document.querySelectorAll("div.sgds-card:not(div." + this.value + ")").forEach(function(el) {
+      console.log(el)
+      el.style.display = "none";
+    });
+  }
+});
