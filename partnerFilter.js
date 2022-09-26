51f89de5f173897dc4f63a7945236deb2757ddc6
Brandon Ong <brandonojy@outlook.sg>
2022-09-26 13:54:47 +0800

    Update partnerFilter.js

--- partnerFilter.js
+++ partnerFilter.js
@@ -34,7 +34,7 @@ Papa.parse('../CoInvPartners.csv', {
 	complete: function(results) {
 		console.log("Finished:", results.data);
 		app.innerHTML = results.data.map(function (each) {
-			return '<div class="sgds-card col agfood" style="flex: 1 1 47%; margin: 10px"> <div class="sgds-card-image" style="margin-top: 15px"> <figure class="sgds-image" style="height: 100px;display: flex;justify-content: center;flex-direction: column;"> <img src="' + each.logo + '" style="object-fit: scale-down; max-width: 100%; max-height: 100%;"> </figure> </div> <div class="sgds-card-content"> <p> <strong>' + each.name + '</strong> </p> <p> <a href="' + each.url + '" target="_blank"> <small> Visit their website </small> </a> </p> </div> </div>';
+			return '<div class="sgds-card col ' + each.category + '" style="flex: 1 1 47%; margin: 10px"> <div class="sgds-card-image" style="margin-top: 15px"> <figure class="sgds-image" style="height: 100px;display: flex;justify-content: center;flex-direction: column;"> <img src="' + each.logo + '" style="object-fit: scale-down; max-width: 100%; max-height: 100%;"> </figure> </div> <div class="sgds-card-content"> <p> <strong>' + each.name + '</strong> </p><small>' + each.desc + '</small><p> <a href="' + each.url + '" target="_blank"> <small> Visit their website </small> </a> </p> </div> </div>';
 		}).join('')
 	}
 });
