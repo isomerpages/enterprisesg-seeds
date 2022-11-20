cf7c92e2981e4f7b7bab40f35db52ac81d447059
Brandon Ong <brandonojy@outlook.sg>
2022-11-20 17:17:37 +0800

    Update coyFilter.js

--- coyFilter.js
+++ coyFilter.js
@@ -43,7 +43,7 @@ Papa.parse('../../Investee.csv', {
 			var aspectratio = allimg[x].naturalWidth / allimg[x].naturalHeight
 			var paddingno = aspectratio * 6
 			var paddingstring = paddingno.toString() + "px"
-			if (aspectratio < 3.5) {
+			if (aspectratio < 4.5) {
 				document.querySelectorAll(".sgds-image")[x].style.padding = paddingstring
 			} else {
 				document.querySelectorAll(".sgds-image")[x].style.padding = "30px"
