dc21844816cd9aa33486a3ffbeb8d4e4e44b5d14
Brandon Ong <brandonojy@outlook.sg>
2022-12-06 12:44:24 +0800

    Update ameFilter.js

--- ameFilter.js
+++ ameFilter.js
@@ -22,7 +22,7 @@ Papa.parse('/Investee.csv', {
 				return each.domain === "ame"
 			})
 			.map(function (each) {
-				return '<div class="sgds-card col ' + each.domain + '" style="flex: 1 1 47%; margin: 10px"> <div class="sgds-card-image" style="margin-top: 15px"> <figure class="sgds-image" style="height: 100px;display: flex;justify-content: center;flex-direction: column;padding: ' + each.padding + 'px"> <img src="' + each.img_url + '" style="object-fit: scale-down; max-width: 100%; max-height: 100%;"> </figure> </div> <div class="sgds-card-content"> <p> <strong>' + each.name + '</strong> </p><small>' + each.desc + '</small><p> <a href="' + each.url + '" target="_blank"> <small> Visit their website </small> </a> </p> </div> </div>';
+				return '<div class="sgds-card col ' + each.domain + '" style="flex: 0 1 47%; margin: 10px"> <div class="sgds-card-image" style="margin-top: 15px"> <figure class="sgds-image" style="height: 100px;display: flex;justify-content: center;flex-direction: column;padding: ' + each.padding + 'px"> <img src="' + each.img_url + '" style="object-fit: scale-down; max-width: 100%; max-height: 100%;"> </figure> </div> <div class="sgds-card-content"> <p> <strong>' + each.name + '</strong> </p><small>' + each.desc + '</small><p> <a href="' + each.url + '" target="_blank"> <small> Visit their website </small> </a> </p> </div> </div>';
 			}).join('')
 	}
 });
