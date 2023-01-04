a6c335a2992f9c18ec7e374f22fe6f1bc7897777
Brandon Ong <brandonojy@outlook.sg>
2023-01-04 01:46:46 +0000

    image resize

--- loadAllInvestee.js
+++ loadAllInvestee.js
@@ -31,4 +31,5 @@ Papa.parse('/Investee.csv', {
 		app.innerHTML = allresults.map(function (each) {
 			return '<div class="sgds-card col ' + each.domain + '" style="flex: 1 1 47%; margin: 10px; max-width: 400px"> <div class="sgds-card-image" style="margin-top: 15px"> <figure class="sgds-image" style="height: 100px;display: flex;justify-content: center;flex-direction: column;"> <img src="' + each.img_url + '" style="object-fit: scale-down; max-width: 70%; max-height: 100%;"> </figure> </div> <div class="sgds-card-content"> <p> <strong>' + each.name + '</strong> </p><small>' + each.desc + '</small><p> <a href="' + each.url + '" target="_blank"> <small> Visit their website </small> </a> </p> </div> </div>';
 		}).join('')
+	}
 });
